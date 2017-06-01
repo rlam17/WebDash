ed11293cfe4c5b022ee6663d8617b5ab2d452cc3
RayLamOffice <RayLamOffice@RAYLAMOFFICE-PC>
2017-06-01 14:24:55 -0400

    Add project files.

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MySqlConnection connect;
+        public MainWindow()
+        {            
+            InitializeComponent();
+            connectToSql();
+        }
+
+        private void connectToSql()
+        {
+            DbConnectionStringBuilder builder = new DbConnectionStringBuilder();
+            builder.Add("Server", "192.168.7.24");
+            builder.Add("Port", "3306");
+            builder.Add("Uid", "wddev_prog");
+            builder.Add("Pwd", "d3Ve7op%#4wd$?");
+            builder.Add("Database", "server_programs");
+
+            connect = new MySqlConnection();
+            connect.ConnectionString = builder.ConnectionString;
+
+            try
+            {
+                connect.Open();
+                connectionLabel.Content = "Connected";
+            } catch(Exception e)
+            {
+                System.Environment.Exit(1);
+            }
+        }
+
+        private void quitButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
