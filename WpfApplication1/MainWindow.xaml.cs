d06ed20175d183fad64313a9313c04a8706fef20
RayLamOffice <RayLamOffice@RAYLAMOFFICE-PC>
2017-06-01 16:00:15 -0400

    TODO: Change ellipse color

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,18 +29,37 @@ namespace WpfApplication1
         public MainWindow()
         {            
             InitializeComponent();
-            
+
+            Ping heartbeat = new Ping();
+            IPAddress sqlAddress = IPAddress.Parse("192.168.7.24");
+            PingReply response = heartbeat.Send(sqlAddress);
+            if(response.Status == IPStatus.Success)
+            {
+                sqlStatus.Content = "Online";
+                sqlStatusLight.Fill = "#00ff1e";
+            } else
+            {
+                sqlStatus.Content = "Offline";
+            }
         }
 
         private void connectButton_Click(object sender, RoutedEventArgs e)
         {
-            Window win2 = new SubWindow();
-            win2.Show();
+            serverLabel.Visibility = Visibility.Visible;
+            serverCombo.Visibility = Visibility.Visible;
+            viewServerButton.Visibility = Visibility.Visible;
+            
         }
 
         private void quitButton_Click(object sender, RoutedEventArgs e)
         {
             System.Environment.Exit(0);
+        }
+
+        private void viewServerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window win2 = new SubWindow();
+            win2.Show();
         }
     }
 
--- WpfApplication1/SubWindow.xaml.cs
+++ WpfApplication1/SubWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace WpfApplication1
         {
             InitializeComponent();
         }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
