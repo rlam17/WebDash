f23a91f5bb412c71bef4b26c0b53b27251bd12e4
RayLamOffice <RayLamOffice@RAYLAMOFFICE-PC>
2017-06-05 13:54:44 -0400

    Started DB create form

---
+++ WpfApplication1/CreateDatabase.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for CreateDatabase.xaml
+    /// </summary>
+    public partial class CreateDatabase : Window
+    {
+        MySqlConnection connect;
+
+        public CreateDatabase(MySqlConnection connect)
+        {
+            this.connect = connect;
+            InitializeComponent();
+        }
+    }
+}
