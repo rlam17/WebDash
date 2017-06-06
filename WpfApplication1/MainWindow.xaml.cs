330fb8147f36af1f7a930a36d30e6b03dd4cc39c
RayLamOffice <RayLamOffice@RAYLAMOFFICE-PC>
2017-06-06 10:25:19 -0400

    Changed db fetch

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -95,7 +95,7 @@ namespace WpfApplication1
         {
             serverCombo.DataContext = null;
 
-            string query = @"SHOW Databases;";
+            string query = @"SELECT table_schema `Database` FROM INFORMATION_SCHEMA.TABLES WHERE table_name='csv_service';";
             MySqlCommand cmd = new MySqlCommand(query, connect);
             MySqlDataAdapter da = new MySqlDataAdapter(cmd);
             DataSet ds = new DataSet();
@@ -143,7 +143,6 @@ namespace WpfApplication1
             catch (Exception ex)
             {
                 return -1;
-
             }
         }
 
